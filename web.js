53879e5b291f974b740e232ab2855edaf79ec28a
JMDomoAdepte <jean-marc.rogi@laposte.net>
2013-12-13 13:33:24 +0100

    ini git

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Coucou World!');
+  res.send('Coucou Le Monde!');
 });
 
 var port = process.env.PORT || 5000;
